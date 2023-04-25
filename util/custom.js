d6018172f4e1576a5c8a8d2fcd4e48db3f7d7473
Emmanuel Browne <emmanuelbrowne751999@gmail.com>
2023-04-25 15:51:40 +0000

    Restructure code base

--- util/custom.js
+++ util/custom.js
@@ -3,30 +3,37 @@
  * @param {value} - class or id attribute value;
  * @returns {node element} - node element
  */
-const element = value => {
-    return document.querySelector(value)
-}
+const element = (value) => {
+  return document.querySelector(value);
+};
 
 /**
  * Removes a class value from an element
  * @param {ele} - node element
  * @param {clas} - Class to remove from the specified html element.
  */
-const remove_class = (ele,clas) => ele.classList.remove(clas)
+const remove_class = (ele, clas) => ele.classList.remove(clas);
+
+/**
+ * Toggle a class value to an element, if the class is avaliable it get removed, else added
+ * @param {ele} - node element
+ * @param {clas} - Class to remove from the specified html element.
+ */
+const toggle_class = (ele, clas) => ele.classList.toggle(clas);
 
 /**
  * Add a class value to an element
  * @param {ele} - node element
  * @param {clas} - Class to be added to the specified html element.
  */
-const add_class = (ele,clas) => ele.classList.add(clas);
+const add_class = (ele, clas) => ele.classList.add(clas);
 
 /**
  * @param {ele} - Query the DOM for specified element and make it visible.
  */
-const show_ele = (ele,val) => ele.style.display = val;
+const show_ele = (ele, val) => (ele.style.display = val);
 
 /**
  * @param {ele} - Query the DOM for specified element and make it hidden.
-*/
-const hide_ele = (ele,val) => ele.style.display = val;
+ */
+const hide_ele = (ele, val) => (ele.style.display = val);
