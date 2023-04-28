9094fab8e19b2e9620df84f87afdb74ec56f86e7
Emmanuel Browne <emmanuelbrowne751999@gmail.com>
2023-04-28 12:58:12 +0000

    working on the responsiveness

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ document.addEventListener("click", (event) => {
   const targetEle = event.target;
   const targetEleClas = Array.from(targetEle.classList);
 
-  if (targetEleClas.includes("angle")) carousal(targetEle, targetEleClas);
+  if (targetEleClas.includes("angle"))
+    targetEleClas.includes("right-arrow")
+      ? carousal("right")
+      : carousal("left");
 });
 
-let currentPosition = 0;
+const heroSlider = findEle(".hero-section").querySelectorAll(".hero-container");
 
-function carousal(targetEle, targetEleClas) {
-  const sliders = targetEle
-    .closest(".hero-section")
-    .querySelectorAll(".hero-container");
+function carousal(direction) {
+  const currentSlide = Array.from(heroSlider).find((ele) =>
+    ele.className.includes("active-hero")
+  );
 
-  toggle_class(sliders[0], slideRight);
+  if (direction === "right") {
+    if (currentSlide.nextElementSibling) {
+      currentSlide.nextElementSibling;
+    }
+  } else {
+    toggle_class(currentSlide, "slide-right");
+    toggle_class(currentSlide, "slide-right");
+    toggle_class(currentSlide.nextElementSibling, "slide-right");
+  }
 }
 
 function slideLeft() {}
--- util/util.js
+++ util/util.js
@@ -1,8 +1,12 @@
 const default_value = {
-    block: 'block',
-    flex: 'flex',
-}
+  block: "block",
+  flex: "flex",
+  direction: {
+    left: "left",
+    right: "right",
+  },
+};
 
 const default_class = {
-    sticky: 'sticky-header'
-}
+  sticky: "sticky-header",
+};
